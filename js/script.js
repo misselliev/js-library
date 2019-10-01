0116d7f2a3a4723c8900b6638523252bbf1ba1fb
Santiago Torres G <thdealer@gmail.com>
2019-10-01 17:16:21 -0500

    refactoring funcitions and fixing checkbox

--- js/script.js
+++ js/script.js
@@ -43,8 +43,10 @@ addBook("JS basics", "Dulce Woof", "425");
 // DOM manipulation
 function renderLibrary() {
   const bookTable = document.querySelector("#books-table");
+  let row;
+
   for (let i = 0; i < bookArray.length; i++) {
-    let row = document.createElement("tr");
+    row = document.createElement("tr");
     bookTable.appendChild(row);
     Object.keys(bookArray[i]).forEach(item => {
       if (item === "index") return;
@@ -52,45 +54,45 @@ function renderLibrary() {
       row.appendChild(cell);
       cell.innerHTML += bookArray[i][item];
     });
-    let cell_button = document.createElement("td");
-    row.appendChild(cell_button);
-    let button_icon = document.createElement("button");
-    cell_button.appendChild(button_icon);
-    let delete_icon = document.createElement("i");
-    button_icon.setAttribute("id", bookArray[i].index);
-    button_icon.className = "delete";
-    button_icon.innerHTML = "Delete ";
-    button_icon.appendChild(delete_icon);
-    delete_icon.className = "alternate trash icon";
+    addDeleteButton(bookArray[i]['index'], row);
   }
+}
+
+function addDeleteButton(book, row) {
+  let cell = document.createElement("td");
+  let button = document.createElement("button");
+  let trash_icon = document.createElement("i");
+  
+  row.appendChild(cell);
+
+  cell.appendChild(button);
+  button.setAttribute("id", book['index']);
+  button.className = "delete";
+  button.innerHTML = "Delete ";
+  
+  trash_icon.className = "alternate trash icon";
+  button.appendChild(trash_icon);
 }
 
 function displayBook(book) {
   const bookTable = document.querySelector("#books-table");
   let row = document.createElement("tr");
   bookTable.appendChild(row);
+  
   Object.keys(book).forEach(item => {
     if (item === "index") return;
     let cell = document.createElement("td");
     row.appendChild(cell);
     cell.innerHTML += book[item];
   });
-  let cell_button = document.createElement("td");
-  row.appendChild(cell_button);
-  let button_icon = document.createElement("button");
-  button_icon.innerHTML = "Delete ";
-  button_icon.className = "delete";
-  cell_button.appendChild(button_icon);
-  let delete_icon = document.createElement("i");
-  button_icon.setAttribute("id", book.index);
-  button_icon.appendChild(delete_icon);
-  delete_icon.className = "alternate trash icon";
+
+  addDeleteButton(book['index'], row)
 }
 
 // Adding delete to buttons
 const bookButtons = document.querySelectorAll("#books-table .delete");
 const listItem = document.querySelectorAll("#books-table");
-Array.from(bookButtons).forEach(function(item) {
+Array.from(bookButtons).forEach(function (item) {
   item.addEventListener("click", e => {
     const a = e.target.parentElement;
     const b = a.parentElement;
@@ -107,11 +109,23 @@ function renderForm() {
     const bookNameVal = form["book-name"].value;
     const bookAuthorVal = form["author-name"].value;
     const bookPagesVal = form["book-pages"].value;
-    const bookStatus = form["book-status"].value;
-    const book = addBook(bookNameVal, bookAuthorVal, bookPagesVal, bookStatus);
-    renderForm();
-    displayBook(book);
+    const bookStatus = form["book-status"].checked ? 'Read' : 'Unread';
+    const res = checkInputs(form);
+
+    if (res) {
+      const book = addBook(bookNameVal, bookAuthorVal, bookPagesVal, bookStatus);
+      displayBook(book);
+      document.getElementById("add-book-form").reset();
+    }
   });
+}
+
+function checkInputs(form) {
+  if (form['book-name'].value == '' || form['author-name'].value == '' || form['book-pages'].value == '') {
+    alert("Fill in all the fields");
+    return false;
+  }
+  return true;
 }
 renderLibrary();
 renderForm();
